f63cebaf7bf74f0b1e1bb4139c10c3490a514ac3
Aymen Gabsi <aymengabsi20@gmail.com>
2025-04-26 20:42:54 +0100

    [Ecommerce-App] Create and configure the discovery server.

---
+++ services/discovery/src/test/java/com/aymengabsi/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aymengabsi.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
