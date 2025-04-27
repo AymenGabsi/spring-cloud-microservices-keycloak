2cca39f5b0de9bcd54e2bd884a925385e9ae8ce9
Aymen Gabsi <aymengabsi20@gmail.com>
2025-04-27 11:05:40 +0100

    [Ecommerce-App] Create the entities and init the database.

---
+++ services/product/src/main/java/com/aymengabsi/ecommerce/category/Category.java
@@ -0,0 +1,27 @@
+package com.aymengabsi.ecommerce.category;
+
+import com.aymengabsi.ecommerce.product.Product;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import jakarta.persistence.Id;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
