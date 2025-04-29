e1e5dd36c84be9c5d2fa4c81e03c5a4b44c054f9
Aymen Gabsi <aymengabsi20@gmail.com>
2025-04-29 06:56:12 +0100

    [Ecommerce-App] Implement the Notification Micro Service.

---
+++ services/notification/src/main/java/com/aymengabsi/ecommerce/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.aymengabsi.ecommerce.notification;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
---
+++ services/notification/src/main/java/com/aymengabsi/ecommerce/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package com.aymengabsi.ecommerce.notification;
+
+public enum NotificationType {
+    ORDER_CONFIRMATION,
+    PAYMENT_CONFIRMATION
+}
